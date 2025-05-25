0b8601f8d246cbe0060f2b6492c96dd8dbf45021
Albright Sunguti <sungutialbright@gmail.com>
2025-05-25 21:25:54 +0300

    changes

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,8 +1,5 @@
-// Import the types from interface.ts
 import { RowID, RowElement } from './interface';
 
-// Declare the CRUD functions
- function insertRow(row: RowElement): RowID;
- function deleteRow(rowId: RowID): void;
- function updateRow(rowId: RowID, row: RowElement): RowID;
-
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
